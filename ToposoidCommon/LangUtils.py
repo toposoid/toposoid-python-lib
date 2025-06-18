e7eac5f6c30370c12226e149153b5f9a586e4fed
makoto-kubodera <makoto.kubodera@linked-ideal.com>
2025-06-18 20:15:28 +0900

    first commit

--- ToposoidCommon/LangUtils.py
+++ ToposoidCommon/LangUtils.py
@@ -3,10 +3,13 @@ import regex
 from .model import DetectedLanguage
 
 JAPANNESE_REGEX = regex.compile(r"^.*([ぁ-ん]|[\u30A1-\u30F4]|\p{sc=Han}).*$")
+SPECIAL_SYMBOL_REGEX1 = regex.compile(r"^NO_REFERENCE_[0-9a-f]{8}\-[0-9a-f]{4}\-[0-9a-f]{4}\-[0-9a-f]{4}\-[0-9a-f]{12}_[0-9]+$")
 
 def detectLangage(text: str):
 
-    if regex.search(JAPANNESE_REGEX, text):
+    if regex.search(SPECIAL_SYMBOL_REGEX1, text):
+        return DetectedLanguage(lang="@@_#1")
+    elif regex.search(JAPANNESE_REGEX, text):
         return DetectedLanguage(lang="ja_JP")
     else:
         lang = detect(text)
@@ -17,3 +20,5 @@ def detectLangage(text: str):
         else:
             raise Exception("This language is not covered by Toposoid")
         
+#hoge = "NO_REFERENCE_5d9afee2-4c10-11f0-9f26-acde48001122_10"
+#print(detectLangage(hoge).lang)
