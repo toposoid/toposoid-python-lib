545cac9fb0a5c957de2ab51931503788e813f60c
makoto-kubodera <makoto.kubodera@linked-ideal.com>
2025-10-03 20:24:48 +0900

    Upgrade to version 0.7-SNAPSHOT

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 '''
   Copyright (C) 2025  Linked Ideal LLC.[https://linked-ideal.com/]
- 
+
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU Affero General Public License as
   published by the Free Software Foundation, version 3.
- 
+
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU Affero General Public License for more details.
- 
+
   You should have received a copy of the GNU Affero General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
@@ -18,17 +18,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='ToposoidCommon',
-    version="0.6",
+    version="0.7-SNAPSHOT",
     description="",
     author='Makoto Kubodera',
     packages=find_packages(),
     license='',
-    package_data={'ToposoidCommon': ['logging.yml']},    
+    package_data={'ToposoidCommon': ['logging.yml']},
     include_package_data=True,
     install_requires=[
         "PyYAML",
         "pydantic",
         "regex",
-        "langdetect"        
+        "langdetect"
     ]
 )
