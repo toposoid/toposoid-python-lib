84ba749cedabc81031a5d65e6406428fa2e24dc3
makoto-kubodera <makoto.kubodera@linked-ideal.com>
2026-03-26 05:34:07 +0000

    https://github.com/toposoid/toposoid-python-lib/issues/7

--- ToposoidCommon/model.py
+++ ToposoidCommon/model.py
@@ -211,11 +211,13 @@ ref. https://github.com/toposoid/toposoid-deduction-protocol-model
 com.ideal.linked.toposoid.protocol.model.base
 '''
 class MatchedKnowledgeNode(BaseModel):
+    propositionId:str
     sentenceId:str 
     nodeId:str 
     caseNameOnEdge:str 
     isDenialWord:bool 
-    nodeType: int    
+    nodeType: int   
+    featureInfoList:List[MatchedFeatureInfo] 
 
 '''
 ref. https://github.com/toposoid/toposoid-deduction-protocol-model
@@ -226,6 +228,8 @@ class CoveredPropositionNode(BaseModel):
     terminalSurface:str
     terminalUrl:str
     matchedKnowledgeNodes:List[MatchedKnowledgeNode]
+    isConfirmed:bool
+    deductionUnit:str
 
 '''
 ref. https://github.com/toposoid/toposoid-deduction-protocol-model
