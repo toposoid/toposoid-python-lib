e67d20f42bc277d1f6eb2444fd7690a17f2ca7e8
makoto-kubodera <makoto.kubodera@linked-ideal.com>
2026-04-22 06:50:51 +0000

    https://github.com/toposoid/toposoid-python-lib/issues/7

--- ToposoidCommon/model.py
+++ ToposoidCommon/model.py
@@ -105,7 +105,6 @@ class KnowledgeFeatureReference(BaseModel):
     source:str = ""
     featureInputType:int = 0    
     extentText:str = "{}"
-    similarity:float = -1.0 
 
 '''
 ref. https://github.com/toposoid/toposoid-knowledgebase-model
